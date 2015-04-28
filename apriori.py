9a1cfea49349ce3535bc98aebd0913e0d50ca4e8
Chao Tang <tctom992@gmail.com>
2015-04-27 23:29:37 -0400

    finish building C1 and L1

--- apriori.py
+++ apriori.py
@@ -7,17 +7,16 @@ import re
 import scipy
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
 from sklearn.naive_bayes import BernoulliNB
 #punctuations
 punc = set(string.punctuation)
 freq = {} #freq dic
-CL = {"yes": 0, "no": 0}
-CPDC = {"yes": {}, "no": {}}
-CPDL = {}
-
-
+featureToIndex = {}
+minsupp = 0.03
+minconf = 0.25
 
 def regex_split(s):
 	rgx = re.compile("([\w][\w']*\w)")
@@ -38,12 +37,60 @@ def clearpunc(string):
 		# if ch in punc:
 		# 	result=result+" "
 	return result
+
 def loadcsv(filename):
 	f=open(filename, "rb")
 	lines = csv.reader(f)
 	dataset = list(lines)
 	f.close()
 	return dataset
+
+def FrequentItemsetGeneration (D, minsupp, selectedWords):
+	itemset_arr = set()  # build 1-itemsets
+	for word in set(selectedWords):
+		itemset_arr.add(frozenset([word]))
+	itemset_arr.add(frozenset(["isPositive"]))
+	itemset_arr.add(frozenset(["isNegative"]))
+
+	C1 = defaultdict(int)
+
+	for record in feature_matrix:
+		for itemset in itemset_arr:
+			test = True
+			for item in itemset:
+				if record[featureToIndex[item]] != 1:
+					test = False
+			if test == True:
+				C1[itemset] += 1
+	
+	L1 = defaultdict(int)
+	# prune out stuffs
+	for itemset, count in C1.items():
+		supp = float(count)/len(D)
+		if supp >= minsupp:
+			L1[itemset] = count
+	print L1
+
+
+
+	# for record in feature_matrix:
+	# 	for i in range(len(selectedWords)):
+	# 		if i == 2000 && record[i] == 1:
+	# 			C1["isPositive"] += 1
+	# 		if i == 2001 && record[i] == 1:
+	# 			C1["isNegative"] += 1
+	# 		if record[i] == 1:
+	# 			C1[selectedWords[i]] += 1
+	return 0
+
+
+# def RuleGeneration (L, minconf):
+
+
+def runApriori(D, minsupp, minconf, selectedWords):
+	L = FrequentItemsetGeneration (D, minsupp, selectedWords)
+	# R = RuleGeneration (L, minconf)
+	# return R
 
 if __name__ == '__main__':
 	review=[]
@@ -66,18 +113,27 @@ if __name__ == '__main__':
 	sortedfreq = sorted(freq.items(), key=operator.itemgetter(1),reverse=True)
 	selected = sortedfreq[100:2100] # drop the top 100 words
 	selectedWords = [item[0] for item in selected]
-	# topwords = set(selectedWords) #for speeding
-	topWordToIndex = {}
+	topwords = set(selectedWords) #for speeding
 	i = 0
 	for word in selectedWords:
-		topWordToIndex[word]=i
+		featureToIndex[word]=i
 		i+=1
-
+	featureToIndex["isPositive"] = 2000
+	featureToIndex["isNegative"] = 2001
+	
+	for i in range(0,100):
+		print "WORD"+str(i+1)+" "+selected[i][0]
 	# build matrix
-	feature_matrix=[[0 for x in range(2500)] for x in range(2000)]
-	for i in range(1, len(dataset)):
-		for word in r:
+	feature_matrix=[[0 for x in range(2002)] for x in range(len(review))]
+	for i in range(0,len(review)):
+		for word in review[i]:
 			if word in topwords:
-				yesmatrix[topWordToIndex[word]][yesindex]+=1
-		yesindex+=1
-	selectedWords
+				feature_matrix[i][featureToIndex[word]]+=1
+		if classlabelset[i]==5: 
+			feature_matrix[i][2000] = 1 # isPositive
+			feature_matrix[i][2001] = 0 # isNegative
+		else:
+			feature_matrix[i][2000] = 0 # isPositive
+			feature_matrix[i][2001] = 1 # isNegative
+
+	runApriori(feature_matrix,minsupp, minconf, selectedWords)
